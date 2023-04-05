2ef9160968ebf0b0866492c79b7f6576015e173e
IhorRykun <ihorrykun@gmail.com>
2023-04-05 19:22:40 +0300

    Initialize gh-pages

---
+++ index.2aa5c7e6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".Problem__contain--svg"),o=document.querySelector(".problem__list"),n=document.querySelector("#arrow-up"),t=document.querySelector("#arrow-down");e.addEventListener("click",(function(){o.classList.toggle("none"),n.classList.toggle("none"),t.classList.toggle("none"),console.log("click")}))}();
+//# sourceMappingURL=index.2aa5c7e6.js.map
---
+++ index.6e65b45b.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".Problem__contain--svg"),o=document.querySelector(".problem__list");const t=document.querySelector("#arrow-up"),c=document.querySelector("#arrow-down");e.addEventListener("click",(function(){o.classList.toggle("none"),t.classList.toggle("none"),c.classList.toggle("none"),console.log("click")}));
+//# sourceMappingURL=index.6e65b45b.js.map
